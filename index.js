52449da2a2c09030632a6bb05bea3212dd39fa40
ErwinAdriel <adrielquispe87@gmail.com>
2025-10-25 21:41:31 -0300

    fix: ejemplo de rutas en index.js

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/ping', (req, res) => {
     res.send('/pong');
 })
 
+app.get('/', (req, res) => {
+    res.send('<h1>Bienvenido</h1>');
+})
+
+app.get('/usuarios', (req, res) => {
+    res.send([
+        {name: 'Adriel', edad: 25},
+        {name: 'Sofia', edad: 21}
+    ]);
+})
+
 app.listen(3000, () => {
     console.log('Servidor en http://localhost:3000/ping');
 })
