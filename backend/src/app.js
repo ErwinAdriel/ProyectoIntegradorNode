ec2d2d6bca2da5d118bfa0fed0d3cab56d618118
ErwinAdriel <adrielquispe87@gmail.com>
2025-11-29 18:26:43 -0300

    fix: new app.js remove index.js

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import productsRouter from './routes/products.routes.js';
+import authRouter from './routes/auth.routes.js';
+import bodyParser from 'body-parser';
+import { authentication } from './middleware/authentication.js';
+
+//App initialization
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+//Routes
+app.use('/api', authentication, productsRouter);
+app.use('/auth', authRouter);
+
+//404 handler
+app.use((req, res, next) => {
+    res.status(404).send('Recurso no encontrado o ruta invalida');
+})
+
+//PORT
+const PORT = 3000;
+
+//Start server
+app.listen(PORT, () => {
+    console.log(`Servidor en http://localhost:${PORT}`);
+})
